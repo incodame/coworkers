9249bed957a4e5423bc352fe614ffae5f38fd062
Incodame <tanguy.ronan@gmail.com>
2018-03-14 03:20:33 +0100

    initial pack

---
+++ pack.pl
@@ -0,0 +1,9 @@
+name(coworkers).
+title('Workflow definition as tree of coroutines').
+version('0.1').
+download('https://github.com/incodame/coworkers/archive/v0.1.zip').
+author('Ronan Tanguy','tanguy.ronan@gmail.com').
+packager('Ronan Tanguy','tanguy.ronan@gmail.com').
+maintainer('Ronan Tanguy','tanguy.ronan@gmail.com').
+home('https://github.com/incodame/coworkers').
+
